ab96e5ba5505c36376997efb4623b0c17e5d2c6e
Fahari576 <fahariwealth442@gmail.com>
2025-07-17 19:57:05 +0300

    Update config.cjs
    Signed-off-by: Fahari576 <fahariwealth442@gmail.com>

--- config.cjs
+++ config.cjs
@@ -3,17 +3,17 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Buddy;;;H4sIAAAAAAAAA61U2Y6jSBD8l3q1Nc1VYCy1NBzGNr6wwedqHwooDpvLVAHGI//7Cnf3zEizO9sr7VuRVEVGZEbmN5DlMcEz3ILhN1CUcY0o7o60LTAYArUKAlyCPvARRWAIVpPpi5DNtpllQ+ayNDW9elGYpCHneRtf+OuOJBhNzs45Y17Bow+Kyk1i7zeAFsO15SqS18tNYVjskrpQ59iRu9HZVYRNyW55UVq0g3pNXsGjQ0RxGWfhqIhwikuUzHBrobj8HH2sj1HGttUcH4xYYpntbrRpKn+plBc+3FtNQLOLdl06Nbv9HH3Hue99eYbUfdtuY2txt5jtdZ722MU14oUx7W3nmr/pJfe78EafxGGG/amPMxrT9tN1x1qpbATjEBx5XFvRRrheVOPu6uObYqRrEvZ689Pmak7XRv454tWG5V1uShh74iWrVjkrt/aaueKJLibVrJ4vw2qm+lKgj5SfiVvlh1cu/6Xu+0V0vrObl8F+3ybOxnKLYnos3cZWjmTZ6iUMVvlxd2LOxuBz9A8VVCdL/pIJ9UpPBwmbetzLxJobO6NoXLH0b/MDe1q8jNPpD/qIVuXvWCbtbRbFbsC1fLvXROaujUR+zDmhuRMgrQ77gWuNVtblYi9r0pMReyg4aZHaTQZV9hieuLtQz8IqV/zjwa7N+uyaUG9en4ouuJ36YMg++qDEYUxoiWicZ12MZ/oA+bWNvRLTZ3WBFBbadDNxps3eWF/3LjO2he1Mz/MxW0l1lRupYNrL1vYd8gr6oChzDxOC/UlMaF62C0wICjEBwz+ejeo0lzjNKTZjHwwBBwUJDiAc8LzwlXxpIkQJKoovGaagD4IyTxcYDGlZ4T54PtBUWdQ0w1BVRuN0FkKD1RVGhpLBKqrOq53C9C2pE6eYUJQWYMhKkJNEnmH4R///4cExvDCSJJaHrKHzMiMyA47XOJmRdUOCGv8vPOTHn32Q4Rt9s/Gz+GwfBHFJ6DariiRH/ofHP34iz8urjNpt5mndAZdg+FMYUxpnIemUVRkqvSiusdbpAMMAJQR/7zcusf+h5X2Habnf2dDhGG7MsSfQce+AfqnNUP61OhnqLgMlxBllGBb0QfJ8ByWOkUUeQmbAQWkof+3Cj++EO3wfUxQnpOvr7MUWi8t4tEJbuFiMx8oxVLRQAT8EfszNuzG11hykdZTf7rK1bQ5OOmlammWTdHKkYg+Hvkwu3Bza9uj1b0DAEMSKGyk7OZNcxRpfuIOeiHO+gS/MgTjNeHPdj7hzZB4GwhLvqJePxvBq9qrTPHSq3aCKG4X1rvnsqpozyN4LexptZqqqvHbZfFzHHv45WbNTvdPVPu+y6MS1Pca7LePdAK4DYcYfIDFQKG4n8+xe1RBmY1USjH3PcRZsT78ma7w7R/wuvYnqqhVZszBhPvL04zPZx0ZJ3jd5/D5s8fMziPFzMb736N9a+ca7Mxzz6P8E8b5p/2FbqbghcWCWAlp4Lw67MBgYDRblCd5gxM1xneWTwDWXcW97icCjc36RIBrkZQqGgKQuAn1Q5lVn32kW5L/JpCnTqb5+U50gQpUfI/HLlHGyzLzdssq8mCASgSFgZ0I0ETp/t0pR2BTRjwkDirJVFOUugsdfCWAu84sIAAA=",
+  SESSION_ID: process.env.SESSION_ID || "Buddy;;;H4sIAAAAAAAAA5VVa5OiOBT9K1v5qjWKvK3qquWptNqiiApb+yFCxDTyMAkiTPnft7C7Z6Z2dmd7+RQScu859557+AryAlM0Qw0YfwUlwVfIULdkTYnAGOjV8YgI6IMYMgjGYOc6J6ua0a2woyiLNH8TzK5qs0hvc37L6/VIyb3DJBwOOe0J3PugrA5nHP0i4LFyVn4e4Vrl7WAZJtNJQER4Omw4RtdzzZRXVjAbXDdKkj6BexcRYoLzxCpPKEMEnmeocSEmn4PfWCY7jnaHvUg8RV1Pw1Y1aESP/K3S/CywZ1aE29sVTafO5+Ab2D15B6/UgzmuniU6sC+hPUxTnzWz3PRTb6W8TCcxJkP6Bp/iJEexE6OcYdZ8uu6Kq7B2dtBPQum522kKNTPaZbYUvKTbLTQD0cc5WlqxuU0+B3wUw20vgZMXnFZ2VQWWGfLZxYaT1il63KusmtetNhNG8qb4EbhLPrSS/p+6W8/GasDHBrLrdTh32Eh5sUWt8n0lQhdt4crVSGyFyjSn6efgRy2pe/vJnF2P4TJ0E3ujjpqE851FOO1N1Go9ydvRRhUFr/4OH7KK/AplWfRunCiV56J1inkOi9vgoDt2fnItRgKPXur2GTvuTE2w2pYbk9ctNcxaz+VLThtQvV0Zk03tG14vENStv7iSptZXTw9GKWqcGIy5ex8QlGDKCGS4yLs9We4DGF89FBHEHtUF0q3YxLm6nFvU2MZzY1XBoLfr2clcU2/CSpV8st6jZEBa/wn0QUmKCFGK4immrCDNAlEKE0TB+I9HozrOBGUFQ8847povCrI65CRBkYa/0y/1CTIKy/JLjhjogyMpsgUCY0Yq1AePC+bQUoWRYHGGJFu6KSuaKimyZYuWrlmiJncMs7ekG5whymBWgjEniyNZ5jhRvP/ZBzm6sTf5dKR5rg+OmFDm51V5LmD8oa2PQxhFRZUzr8kjo1sgAsbD79uIMZwntGNW5ZBEJ3xFRscDjI/wTNG3OiOC4g8u795hFHHXfnu93KwUnQcd9i7QT7UZ8+rP5clh9zVY7l5+05eeB/rg/LjIcUNVFXmB5xRZVeXubndw/4a5SxEjBvGZds7hugp2Bd2av/riYjGZaFaiGYkGvnP8kOybJpArhGnT8ut8P72pF5SHVil4GM3WU9FY7Oy2TObTueEom4cm/h4EjMHKH72uLu11O1idqzMdDm45nxI7scggglF9XeO0kE7KcL9uK3PfKNL1uo3SQPWbxhjGN//Zg5drkx2WnjLdaeu6Kp4ls37qssXoiiP0YzLe2SEv2UlysLo0dRmLy8PB4g7DMPJ4/pKXry72Dm5h0v1mx28gLhyOTGzulN2WssTJejgpsguyn+MXmts3kfmLKtbqt2F6DPP53UTxu87x4/WI0cOT3rv0n918A96Jbnjv/xDj3eX+xSn0/WtvEi6oOgy5m3KEeTlaz5BV5Dhd91av9imSAuPW1Ni1OHDv1F+eITsWJANjQLMDBH1AiqqTsJMfi1/9YDTHMd895Awp076PxT9MGq++feWSopxCegJj4O5SUe8k3mhl6THIPoYMaN0zhwdw/wuL1YuzBggAAA==",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT !== undefined ? process.env.AUTO_STATUS_REACT === 'true' : true, 
   AUTO_BIO: process.env.AUTO_BIO !== undefined ? process.env.AUTO_BIO === 'true' : true,
-  AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
+  AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : true,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
   AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
-  AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : false,
-  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : false,
-  AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
+  AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : true,
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : true,
+  AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : true,
    /*auto block only for 212 */
   AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
   
@@ -25,7 +25,7 @@ const config = {
   OWNER_NAME: process.env.OWNER_NAME || "Carl William",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "254101617718",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
-  WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
+  WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : true, 
 };
 
 
